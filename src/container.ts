d970472afb14237a2eb9e11c5e8be75e458e4eae
SalathielGenese <salathielgenese@gmail.com>
2021-08-20 05:31:25 +0400

    fix: revert to VSCode default formatting

--- src/container.ts
+++ src/container.ts
@@ -11,63 +11,63 @@ export class Container {
     #values = new Map<Token<any> | Constructor, any>();
     #factories = new Map<Token<any>, ValueFactory<any>>();
 
-    static #isToken( token: any ): token is Token<any> {
+    static #isToken(token: any): token is Token<any> {
         return 'symbol' === typeof token;
     }
 
-    register<T>( token: Constructor<T> ): this;
-    register<T>( token: Token<T>, factory: ValueFactory<T> ): this;
-    register<T>( token: Constructor<T>, value: any ): this;
-    register<T>( token: Token<T> | Constructor<T>, factoryOrValue?: any ): this {
-        if ( Container.#isToken( token ) ) {
-            this.#factories.set( token, factoryOrValue! );
-        } else if ( 1 < arguments.length ) {
-            this.#values.set( token, factoryOrValue );
+    register<T>(token: Constructor<T>): this;
+    register<T>(token: Token<T>, factory: ValueFactory<T>): this;
+    register<T>(token: Constructor<T>, value: any): this;
+    register<T>(token: Token<T> | Constructor<T>, factoryOrValue?: any): this {
+        if (Container.#isToken(token)) {
+            this.#factories.set(token, factoryOrValue!);
+        } else if (1 < arguments.length) {
+            this.#values.set(token, factoryOrValue);
         } else {
-            this.#constructors.add( token );
+            this.#constructors.add(token);
         }
 
         return this;
     }
 
-    async inject<TS extends (Token<any> | Constructor)[]>( ...tokens: TS ): Promise<{
+    async inject<TS extends (Token<any> | Constructor)[]>(...tokens: TS): Promise<{
         [K in keyof TS]: TS[K] extends Token<infer TT>
-            ? TT extends Promise<infer T>
-                ? T
-                : TT
-            : TS[K] extends Constructor<infer CT>
-                ? CT extends Promise<infer T>
-                    ? T
-                    : CT
-                : never;
+        ? TT extends Promise<infer T>
+        ? T
+        : TT
+        : TS[K] extends Constructor<infer CT>
+        ? CT extends Promise<infer T>
+        ? T
+        : CT
+        : never;
     }> {
         const values: any[] = [];
 
-        for ( const token of tokens ) {
-            if ( this.#values.has( token ) ) {
-                values.push( this.#values.get( token ) );
-            } else if ( Container.#isToken( token ) && this.#factories.has( token ) ) {
+        for (const token of tokens) {
+            if (this.#values.has(token)) {
+                values.push(this.#values.get(token));
+            } else if (Container.#isToken(token) && this.#factories.has(token)) {
                 try {
                     // NOTE: catch exception for sync & async factory
-                    const value = await this.#factories.get( token )!( this );
-                    values.push( this.#values.set(token, value).get(token) );
-                } catch ( error ) {
-                    throw new Container.TargetComputeError( token, error );
+                    const value = await this.#factories.get(token)!(this);
+                    values.push(this.#values.set(token, value).get(token));
+                } catch (error) {
+                    throw new Container.TargetComputeError(token, error);
                 }
-            } else if ( !Container.#isToken( token ) ) {
-                if ( !this.#constructors.has( token ) ) {
-                    throw new Container.TargetNotFound( token );
+            } else if (!Container.#isToken(token)) {
+                if (!this.#constructors.has(token)) {
+                    throw new Container.TargetNotFound(token);
                 }
 
                 try {
                     // NOTE: catch exception for sync & async factory
-                    const value = Reflect.construct( token, [ this ] );
-                    values.push( this.#values.set( token, value ).get( token ) );
-                } catch ( error ) {
-                    throw new Container.TargetComputeError( token, error );
+                    const value = Reflect.construct(token, [this]);
+                    values.push(this.#values.set(token, value).get(token));
+                } catch (error) {
+                    throw new Container.TargetComputeError(token, error);
                 }
             } else {
-                throw new Container.TargetNotFound( token );
+                throw new Container.TargetNotFound(token);
             }
         }
 
@@ -75,18 +75,18 @@ export class Container {
     }
 
     static TargetComputeError = class TargetNotFound extends Error {
-        constructor( token: Token<any> | Constructor, public readonly cause: any ) {
-            super( `Could not resolve target value from Container: ${ token.toString() }` );
+        constructor(token: Token<any> | Constructor, public readonly cause: any) {
+            super(`Could not resolve target value from Container: ${token.toString()}`);
         }
     };
 
     static TargetNotFound = class TargetNotFound extends Error {
-        constructor( token: Token<any> | Constructor ) {
-            super( `Could not resolve target value from Container: ${ token.toString() }` );
+        constructor(token: Token<any> | Constructor) {
+            super(`Could not resolve target value from Container: ${token.toString()}`);
         }
     };
 }
 
 export interface ValueFactory<T> {
-    ( container: Container ): T | Promise<T>;
+    (container: Container): T | Promise<T>;
 }
