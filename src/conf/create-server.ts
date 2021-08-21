a3155c6539adfc72cb10285bdcc268564f50d655
SalathielGenese <salathielgenese@gmail.com>
2021-08-21 18:59:50 +0400

    feat: handle validation error

--- src/conf/create-server.ts
+++ src/conf/create-server.ts
@@ -1,6 +1,7 @@
 import { SERVER_HOST, SERVER_PORT } from './create-env';
 import { Container } from './../container';
 import { Server } from "@hapi/hapi";
+import { ValidationError } from 'joi';
 
 export const SERVER_TOKEN: Token<Server> = Symbol('Server token');
 
@@ -8,4 +9,15 @@ export const createServer = (container: Container) =>
     container.register(SERVER_TOKEN, async () => new Server({
         host: (await container.inject(SERVER_HOST))[0],
         port: (await container.inject(SERVER_PORT))[0],
+        routes: {
+            validate: {
+                async failAction(_request, h, error) {
+                    if (error instanceof ValidationError) {
+                        return h.response(error.details).code(400).takeover();
+                    }
+
+                    throw error;
+                }
+            }
+        },
     }));
