9ed21063f950b7a86eb3dc53d50e9031053b78ca
SalathielGenese <salathielgenese@gmail.com>
2021-08-22 13:23:41 +0400

    feat: separate setup from starting, to reuse cold app in tests

---
+++ src/conf/app.ts
@@ -0,0 +1,28 @@
+import { clients } from './../app/api/clients';
+import { dispatches } from './../app/api/dispatches';
+import { matches } from './../app/api/matches';
+import { requests } from './../app/api/requests';
+import { users } from './../app/api/users';
+import { ClientService } from './../app/service/client.service';
+import { DispatchService } from './../app/service/dispatch.service';
+import { Logger } from './../app/service/logger';
+import { MatchService } from './../app/service/match.service';
+import { RequestService } from './../app/service/request.service';
+import { UserService } from './../app/service/user.service';
+import { Container } from './../container';
+import { createEnv } from './create-env';
+import { createRedis } from './create-redis';
+import { createServer } from './create-server';
+import { createServerPlugin } from './create-server-plugin';
+
+export const app = (container = new Container()): Container =>
+    container
+        .visit(createEnv, createRedis, createServer, createServerPlugin)
+        .visit(dispatches, requests, matches, clients, users)
+        .register(DispatchService)
+        .register(RequestService)
+        .register(ClientService)
+        .register(MatchService)
+        .register(UserService)
+        .register(Logger)
+    ;
--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,22 @@
-import { clients } from './app/api/clients';
-import { dispatches } from './app/api/dispatches';
-import { matches } from './app/api/matches';
-import { requests } from './app/api/requests';
-import { users } from './app/api/users';
-import { ClientService } from './app/service/client.service';
-import { DispatchService } from './app/service/dispatch.service';
 import { Logger } from './app/service/logger';
-import { MatchService } from './app/service/match.service';
-import { RequestService } from './app/service/request.service';
-import { UserService } from './app/service/user.service';
-import { createEnv } from './conf/create-env';
-import { createRedis, REDIS_TOKEN } from './conf/create-redis';
-import { createServer, SERVER_TOKEN } from './conf/create-server';
-import { createServerPlugin } from './conf/create-server-plugin';
-import { Container } from './container';
+import { app } from './conf/app';
+import { REDIS_TOKEN } from './conf/create-redis';
+import { SERVER_TOKEN } from './conf/create-server';
 
-(async container => {
+(async (log?: Logger) => {
     try {
+        const [logger, server, redis] = await app().inject(Logger, SERVER_TOKEN, REDIS_TOKEN);
+
+        log = logger;
+        await server.start();
+        logger.log(`Started ${server.info.uri}`);
         process.on('SIGINT', () => {
             server.stop();
             redis.disconnect(false);
             logger.log(`Stopped ${server.info.uri}, closed Redis, gracefully shutdown!`);
             process.exit(130);
         });
-        container.visit(createEnv, createRedis, createServer, createServerPlugin)
-            .visit(dispatches, requests, matches, clients, users)
-            .register(DispatchService)
-            .register(RequestService)
-            .register(ClientService)
-            .register(MatchService)
-            .register(UserService)
-            .register(Logger);
-
-        const [logger, server, redis] = await container.inject(Logger, SERVER_TOKEN, REDIS_TOKEN);
-
-        await server.start();
-        logger.log(`Started ${server.info.uri}`);
     } catch (error) {
-        (await container.inject(Logger))[0].error(error);
+        (log ?? console).error(error);
     }
-})(new Container());
+})();
