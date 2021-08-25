9f25912e11d82e81e22b7f9f138f010f6f4e4371
SalathielGenese <salathielgenese@gmail.com>
2021-08-25 10:10:25 +0400

    feat: define `clientId` existence checker for reusability

--- src/app/api/util.validator.ts
+++ src/app/api/util.validator.ts
@@ -1,3 +1,4 @@
+import { ClientService } from './../service/client.service';
 import Joi, { ExternalValidationFunction } from 'joi';
 import { Container } from './../../container';
 import { UserService } from './../service/user.service';
@@ -20,4 +21,25 @@ export const userIdExists = (container: Container, optional = false): ExternalVa
                 },
             }], userId)
         }
-    }
+    };
+
+
+export const clientIdExists = (container: Container, optional = false): ExternalValidationFunction =>
+    async clientId => {
+        if (optional && void 0 === clientId) return;
+
+        const [clientService] = await container.inject(ClientService);
+
+        if (!await clientService.exists(clientId)) {
+            throw new Joi.ValidationError('clientId.notFound', [{
+                message: '"clientId" does not exists',
+                type: 'clientId.notFound',
+                path: ["clientId"],
+                context: {
+                    value: clientId,
+                    label: 'clientId',
+                    key: 'clientId',
+                },
+            }], clientId)
+        }
+    };
