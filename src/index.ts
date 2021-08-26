396b48bbfb41964d292e9883a45cdb3a9b739287
SalathielGenese <salathielgenese@gmail.com>
2021-08-26 14:21:00 +0400

    feat: account for some completed steps in graceful shutdown

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,26 @@
+import { Server } from '@hapi/hapi';
+import { Redis } from 'ioredis';
 import { Logger } from './app/service/logger';
 import { app } from './conf/app';
+import { MIGRATOR_TOKEN } from './conf/create-migrator';
 import { REDIS_TOKEN } from './conf/create-redis';
 import { SERVER_TOKEN } from './conf/create-server';
 
-(async (log?: Logger) => {
+(async (logger?: Logger, server?: Server, redis?: Redis, migrator?: () => Promise<void>) => {
     try {
-        const [logger, server, redis] = await app().inject(Logger, SERVER_TOKEN, REDIS_TOKEN);
-
-        log = logger;
+        ([logger, server, redis, migrator] = await app().inject(Logger, SERVER_TOKEN, REDIS_TOKEN, MIGRATOR_TOKEN));
+        await migrator();
         await server.start();
         logger.log(`Started ${server.info.uri}`);
         process.on('SIGINT', () => {
-            server.stop();
-            redis.disconnect(false);
-            logger.log(`Stopped ${server.info.uri}, closed Redis, gracefully shutdown!`);
+            server!.stop();
+            redis!.disconnect(false);
+            logger!.log(`Stopped ${server!.info.uri}, closed Redis, gracefully shutdown!`);
             process.exit(130);
         });
     } catch (error) {
-        (log ?? console).error(error);
+        server?.stop();
+        redis?.disconnect(false);
+        (logger ?? console).error(error);
     }
 })();
