8cff7c647c1a15034a19f1597ca22ea0d765f121
SalathielGenese <salathielgenese@gmail.com>
2021-09-14 10:38:26 +0400

    fix: document field as optional

--- src/app/model/blood-donor.ts
+++ src/app/model/blood-donor.ts
@@ -3,6 +3,6 @@ import { BloodGroup } from "./blood-group";
 import { RhesusFactor } from "./rhesus-factor";
 
 export interface BloodDonor extends Model {
-    rhesusFactor: RhesusFactor;
+    rhesusFactor?: RhesusFactor;
     bloodGroup: BloodGroup;
 }
