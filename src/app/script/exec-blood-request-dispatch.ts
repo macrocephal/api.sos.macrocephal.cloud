6e9bed21b248db9e817c642dc4b3527a25812da3
SalathielGenese <salathielgenese@gmail.com>
2021-09-21 19:11:45 +0400

    feat: remove trace logging

--- src/app/script/exec-blood-request-dispatch.ts
+++ src/app/script/exec-blood-request-dispatch.ts
@@ -3,7 +3,6 @@ import { RhesusFactor } from '../model/rhesus-factor';
 import { REDIS_TOKEN } from './../../conf/create-redis';
 import { Container } from './../../container';
 import { BloodDispatch } from './../model/blood-dispatch';
-import { Logger } from './../service/logger';
 import { withRedis } from './with-redis';
 
 /**
@@ -19,7 +18,7 @@ export const execBloodRequestDispatch = async ({
     const BLOOD_COORDINATES = 'donors:blood:coordinates';
     const NEIGHBOURHOOD = `neighbourhood:${dispatchId}`;
 
-    const [redis, logger] = await container.inject(REDIS_TOKEN, Logger);
+    const [redis] = await container.inject(REDIS_TOKEN);
     let dispatch: BloodDispatch;
 
     switch (bloodGroup) {
@@ -29,39 +28,20 @@ export const execBloodRequestDispatch = async ({
             const REQUEST_O_RHESUS = `request-matches:blood:${requestId}:${bloodGroup}:${rhesusFactor}`;
             const REQUEST_O = `request-matches:blood:${requestId}:${bloodGroup}`;
 
-            await logger.trace(`${BLOOD_COORDINATES} >>>`, await redis.zrange(BLOOD_COORDINATES, 0, -1, 'WITHSCORES'));
             await redis.send_command('GEORADIUS', BLOOD_COORDINATES, longitude, latitude, 50_000, 'm', 'ASC', 'STOREDIST', NEIGHBOURHOOD);
-            await logger.trace(`${NEIGHBOURHOOD} >>>`, await redis.zrange(NEIGHBOURHOOD, 0, -1, 'WITHSCORES'));
-            // {DISPATCH} Find all group O<rhesus> in the neighbourhood
             await redis.zinterstore(DISPATCH_O_RHESUS, 3, NEIGHBOURHOOD, BLOOD_GROUP, RHESUS_FACTOR);
-            // {DISPATCH} Retain distinct matches between this dispatch and overall request matches
-            await logger.trace(`${DISPATCH_O_RHESUS} >>>`, await redis.zrange(DISPATCH_O_RHESUS, 0, -1, 'WITHSCORES'));
             await withRedis(redis).ZDIFFSTORE(DISPATCH_O_RHESUS, DISPATCH_O_RHESUS, REQUEST_O_RHESUS, REQUEST_O);
-            // {DISPATCH} Remove requester, if the latter happens to be a matching donor as well
             await redis.zrem(DISPATCH_O_RHESUS, userId);
-            await logger.trace(`${DISPATCH_O_RHESUS} >>>`, await redis.zrange(DISPATCH_O_RHESUS, 0, -1, 'WITHSCORES'));
-
             // TODO: Maybe cap DISPATCH_O_RHESUS !???
-            // {REQUEST} Merge this dispatch matches with request's
             await redis.send_command('ZUNIONSTORE', REQUEST_O_RHESUS, 2, REQUEST_O_RHESUS, DISPATCH_O_RHESUS);
-            await logger.trace(`${REQUEST_O_RHESUS} >>>`, await redis.zrange(REQUEST_O_RHESUS, 0, -1, 'WITHSCORES'));
 
             const countBefore = await Promise.all([redis.zcard(REQUEST_O_RHESUS), redis.zcard(REQUEST_O)]).then(([a, b]) => +a + b);
 
             if (countBefore < 50) {
-                // {DISPATCH} Find all group O in the neighbourhood
                 await redis.zinterstore(DISPATCH_O, 2, NEIGHBOURHOOD, BLOOD_GROUP);
-                await logger.trace(`${DISPATCH_O} >>>`, await redis.zrange(DISPATCH_O, 0, -1, 'WITHSCORES'));
-                // {DISPATCH} Retain distinct matches between this dispatch and overall request matches
                 await withRedis(redis).ZDIFFSTORE(DISPATCH_O, DISPATCH_O, REQUEST_O_RHESUS, REQUEST_O); // HAHAHA
-                await logger.trace(`${DISPATCH_O} >>>`, await redis.zrange(DISPATCH_O, 0, -1, 'WITHSCORES'));
-                // {DISPATCH} Remove requester, if the latter happens to be a matching donor as well
                 await redis.zrem(DISPATCH_O, userId);
-                await logger.trace(`${DISPATCH_O} >>>`, await redis.zrange(DISPATCH_O, 0, -1, 'WITHSCORES'));
-                // {REQUEST} Merge this dispatch matches with request's
                 await redis.send_command('ZUNIONSTORE', REQUEST_O, 2, REQUEST_O, DISPATCH_O);
-                await logger.trace(`${REQUEST_O} >>>`, await redis.zrange(REQUEST_O, 0, -1, 'WITHSCORES'));
-                // TODO: Maybe cap DISPATCH_O !???
             }
 
             dispatch = await Promise.all([
