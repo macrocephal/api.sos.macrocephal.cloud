86dec829cab5365f8da8c81cf11fc7392d5b4ef7
SalathielGenese <salathielgenese@gmail.com>
2021-09-22 15:33:07 +0400

    feat: implement BloodDonorService.update action

--- src/app/service/blood-donor.service.ts
+++ src/app/service/blood-donor.service.ts
@@ -1,3 +1,4 @@
+import { Pipeline } from 'ioredis';
 import { BloodGroup } from '../model/blood-group';
 import { RhesusFactor } from '../model/rhesus-factor';
 import { Container } from './../../container';
@@ -30,6 +31,35 @@ export class BloodDonorService extends WithApplication {
         ]);
 
         this.logger.debug('BloodDonorService.create >>> ', donor);
+        return donor;
+    }
+
+    async update(userId: string, payload: { bloodGroup: BloodGroup, rhesusFactor?: RhesusFactor }): Promise<BloodDonor> {
+        this.logger.debug('BloodDonorService.update <<< ', ...arguments);
+        let donor = (await this.#donors.doc(userId).get()).data();
+
+        if (!donor) throw new Error('BLOOD_DONOR_NOT_FOUND');
+
+        let rem: Pipeline | undefined = undefined;
+        let add: Pipeline | undefined = undefined;
+        const bloodGroupKey = this.key.donors.blood.group(donor.bloodGroup);
+        const rhesusFactorKey = this.key.donors.blood.rhesus(donor.rhesusFactor!);
+
+        if (donor.bloodGroup !== payload.bloodGroup) {
+            rem = this.redis.pipeline().srem(bloodGroupKey, userId);
+            add = this.redis.pipeline().sadd(bloodGroupKey, userId);
+        }
+
+        if (donor.rhesusFactor !== payload.rhesusFactor) {
+            rem = (rem ?? this.redis.pipeline()).srem(rhesusFactorKey, userId);
+            add = (add ?? this.redis.pipeline()).sadd(rhesusFactorKey, userId);
+        }
+
+        donor = { ...donor, ...payload };
+        await rem;
+        await Promise.all([this.#donors.doc(donor.id).set(donor, { merge: true }), add]);
+
+        this.logger.debug('BloodDonorService.update >>> ', donor);
         return donor;
     }
 }
