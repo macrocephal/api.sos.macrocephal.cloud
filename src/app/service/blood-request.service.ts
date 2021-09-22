7799c7e498c78188650d01e3207bd98183b4fd86
SalathielGenese <salathielgenese@gmail.com>
2021-09-22 18:36:40 +0400

    feat: implement BloodRequestService.create & BloodRequestService.dispatch

--- src/app/service/blood-request.service.ts
+++ src/app/service/blood-request.service.ts
@@ -1,6 +1,10 @@
+import { v4 } from 'uuid';
+import { BloodGroup } from '../model/blood-group';
+import { RhesusFactor } from '../model/rhesus-factor';
 import { Container } from './../../container';
 import { BloodRequest } from './../model/blood-request';
 import { BloodRequestDispatch } from './../model/blood-request-dispatch';
+import { execBloodRequestDispatch } from './../script/exec-blood-request-dispatch';
 import { WithApplication } from './../with-application';
 
 export class BloodRequestService extends WithApplication {
@@ -21,5 +25,72 @@ export class BloodRequestService extends WithApplication {
                     toFirestore: model => model,
                 });
         })();
+    }
+
+    async search(userId: string, requestId: string): Promise<BloodRequest> {
+        const request = (await this.#bloodRequests
+            .where('id', '==', requestId)
+            .where('userId', '==', userId)
+            .get()).docs[0]?.data();
+
+        if (!request) {
+            const error = new Error(`Blood request not found: ${requestId} [userId=${userId}]`);
+
+            error.name = WithApplication.ERROR_NOT_FOUND;
+
+            throw error;
+        }
+
+        return request;
+    }
+
+    async create(userId: string, payload: { bloodGroup: BloodGroup, rhesusFactor: RhesusFactor, longitude: number, latitude: number }): Promise<BloodRequest> {
+        const { rhesusFactor, bloodGroup, longitude, latitude } = payload;
+        this.logger.debug('BloodRequestService.create <<< ', ...arguments);
+        const request: BloodRequest = { rhesusFactor, bloodGroup, createdAt: Date.now(), active: true, id: v4(), userId };
+
+        await this.#bloodRequests.doc(request.id).set(request, { merge: false });
+
+        try {
+            await this.dispatch(userId, request.id, { longitude, latitude });
+        } catch (error: any) {
+            await this.#bloodRequests.doc(request.id).delete();
+            error.name = WithApplication.ERROR_CONFLICT;
+            throw error;
+        }
+
+        this.logger.debug('BloodRequestService.create >>> ', request);
+        return request;
+    }
+
+    async dispatch(userId: string, requestId: string, payload: { longitude: number, latitude: number }): Promise<BloodRequestDispatch> {
+        this.logger.debug('BloodRequestService.dispatch <<< ', ...arguments);
+        const request = await this.search(userId, requestId);
+
+        if (!request.active) {
+            const error = new Error(`Blood request not active: ${requestId}`);
+
+            error.name = WithApplication.ERROR_CONFLICT;
+
+            throw error;
+        }
+
+        // TODO: pull all dispatches for this request, merge them and hydrate Redis
+        const dispatch = await execBloodRequestDispatch({
+            rhesusFactor: request.rhesusFactor,
+            bloodGroup: request.bloodGroup,
+            longitude: payload.longitude,
+            latitude: payload.latitude,
+            container: this.container,
+            requestId: request.id,
+            dispatchId: v4(),
+            userId,
+        });
+
+        await this.#bloodRequestDispatches.doc(dispatch.id).set(dispatch, { merge: false });
+        // TODO: send notifications to matches for this last dispatch
+
+        this.logger.debug('BloodRequestService.dispatch >>> ', dispatch);
+        return dispatch;
     }
 }
